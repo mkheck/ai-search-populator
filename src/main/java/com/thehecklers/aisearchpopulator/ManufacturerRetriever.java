f36ef93a98d96a4504a9a77493f745a1794faf6b
Mark Heckler <mark.heckler@gmail.com>
2024-02-01 17:18:30 -0600

    Cleanup, streamline, extract manufacturer name

--- src/main/java/com/thehecklers/aisearchpopulator/ManufacturerRetriever.java
+++ src/main/java/com/thehecklers/aisearchpopulator/ManufacturerRetriever.java
@@ -1,32 +1,36 @@
 package com.thehecklers.aisearchpopulator;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 import jakarta.annotation.PostConstruct;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class ManufacturerRetriever {
     @Value("${aircraft.url}")
     private String aircraftUrl;
-
     @Value("${aircraft.application.id}")
     private String appId;
-
     @Value("${aircraft.api.key}")
     private String apiKey;
+
+    private final RestClient client = RestClient.create();
+
+    private final List<String> manufacturers = new ArrayList<>();
 
     @PostConstruct
     public void retrieve() {
         try {
-            URL url = new URL(aircraftUrl);
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestProperty("X-Parse-Application-Id", appId); // This is your app's application id
+            HttpURLConnection urlConnection = (HttpURLConnection) new URL(aircraftUrl).openConnection();
+            urlConnection.setRequestProperty("X-Parse-Application-Id", appId);
             urlConnection.setRequestProperty("X-Parse-REST-API-Key", apiKey);
             try {
                 BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
@@ -35,8 +39,13 @@ public class ManufacturerRetriever {
                 while ((line = reader.readLine()) != null) {
                     stringBuilder.append(line);
                 }
-                JSONObject data = new JSONObject(stringBuilder.toString()); // Here you have the data that you need
-                System.out.println(data.toString(2));
+                JSONObject data = new JSONObject(stringBuilder.toString());
+                data.getJSONArray("results").forEach(o -> {
+                    JSONObject manufacturer = (JSONObject) o;
+                    manufacturers.add(manufacturer.getString("Manufacturer"));
+                });
+                manufacturers.forEach(System.out::println);
+                //System.out.println(data.toString(2));
             } finally {
                 urlConnection.disconnect();
             }
