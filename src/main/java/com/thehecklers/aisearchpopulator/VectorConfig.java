29b52850872702e2e4c3a14fca641f18b0f81011
Mark Heckler <mark.heckler@gmail.com>
2024-02-07 17:12:04 -0600

    Azure AI Search

--- src/main/java/com/thehecklers/aisearchpopulator/VectorConfig.java
+++ src/main/java/com/thehecklers/aisearchpopulator/VectorConfig.java
@@ -1,25 +1,38 @@
 package com.thehecklers.aisearchpopulator;
 
-import org.neo4j.driver.AuthTokens;
-import org.neo4j.driver.Driver;
-import org.neo4j.driver.GraphDatabase;
+import com.azure.core.credential.AzureKeyCredential;
+import com.azure.search.documents.indexes.SearchIndexClient;
+import com.azure.search.documents.indexes.SearchIndexClientBuilder;
 import org.springframework.ai.embedding.EmbeddingClient;
-import org.springframework.ai.openai.OpenAiEmbeddingClient;
-import org.springframework.ai.openai.api.OpenAiApi;
-import org.springframework.ai.vectorstore.Neo4jVectorStore;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.azure.AzureVectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class VectorConfig {
+    @Bean
+    public SearchIndexClient searchIndexClient() {
+        return new SearchIndexClientBuilder().endpoint(System.getenv("AZURE_AI_SEARCH_ENDPOINT"))
+                .credential(new AzureKeyCredential(System.getenv("AZURE_AI_SEARCH_API_KEY")))
+                .buildClient();
+    }
+
+    @Bean
+    public VectorStore vectorStore(SearchIndexClient searchIndexClient, EmbeddingClient embeddingClient) {
+        return new AzureVectorStore(searchIndexClient, embeddingClient);
+    }
+
 /*
+    // PostgreSQL / PGvector version
     @Bean
     public VectorStore vectorStore(JdbcTemplate jdbcTemplate, EmbeddingClient embeddingClient) {
         return new PgVectorStore(jdbcTemplate, embeddingClient);
     }
 */
 
+/*
+    // Neo4j version
     @Bean
     public VectorStore vectorStore(Driver driver, EmbeddingClient embeddingClient) {
         return new Neo4jVectorStore(driver, embeddingClient,
@@ -37,4 +50,5 @@ public class VectorConfig {
     public EmbeddingClient embeddingClient() {
         return new OpenAiEmbeddingClient(new OpenAiApi(System.getenv("OPENAI_API_KEY")));
     }
+*/
 }
